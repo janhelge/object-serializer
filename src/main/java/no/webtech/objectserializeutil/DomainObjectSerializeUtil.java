e84930b6c984b1761b7f5276a5f76cab40ac44cc
Jan Helge Salvesen <Jan.Helge.Salvesen@gmail.com>
2017-09-05 19:47:59 +0200

    Forenklet

--- src/main/java/no/webtech/objectserializeutil/DomainObjectSerializeUtil.java
+++ src/main/java/no/webtech/objectserializeutil/DomainObjectSerializeUtil.java
@@ -1,5 +1,7 @@
 package no.webtech.objectserializeutil;
 
+import no.webtech.objectserializeutil.impl.SerializableObjectHelper;
+import no.webtech.objectserializeutil.impl.SnapperUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,56 +11,31 @@ public class DomainObjectSerializeUtil {
 	
 	private static Logger logger = LoggerFactory.getLogger(DomainObjectSerializeUtil.class);
 
-    public static String serializedObjectFileName(String uniqFilenameBase) {
-        // TODO: Lag denne med ENV-variable for docker-config
-        //return System.getProperty("supersnap.dir", "src/test/data/snap_") + uniqFilenameBase + ".ser";
-        return "/tmp/snap_" + uniqFilenameBase + ".ser";
-    }
-
 	public static Serializable loadFromFile(String fileName) {
 		byte[] ba = readWholeFileAsBytes(fileName);
 		logger.debug("File: " + fileName + " Loaded buffer size: " + (ba!=null? ba.length:"<ba is null>"));
 		if (ba == null) return null;
-		return (Serializable) SerializableObjectHelper.createInstance(ba, 0).getObject();		
+		return (Serializable) SerializableObjectHelper.createInstance(ba, 0).getObject();
 	}
 
+    public static void dumpToFile(Serializable serializableObject, File f, String comment) {
+        StringBuffer sb = SnapperUtil.createSerializedFormattedOutput(serializableObject);
+        OutputStream os;
+        try {
+            os = new FileOutputStream(f);
+            if (comment != null) {
+                os.write(comment.getBytes());
+                os.write('\n');
+            }
+            os.write(sb.toString().getBytes());
+            os.flush();
+            os.close();
+            logger.debug("Created file " + f.getPath());
+        } catch (IOException e) {
+            logger.error("dumpToFile(Serializable rf, String id, String comment): "+stringifyException(e));
+        }
+    }
 
-	public static void dumpToFile(Serializable serializableObject, String uniqFilenameBase, String comment) {
-		StringBuffer sb = SnapperUtil.createSerializedFormattedOutput(serializableObject);
-		String fn = serializedObjectFileName(uniqFilenameBase);
-		File f = new File(fn);
-		if (f.exists()){
-			logger.warn("Warning: File exists, will be replaced");
-		}
-		
-		// TODO: Fix if more than last directory does not exist...
-		// Create directory for f if not exists
-		File dirn = f.getParentFile();
-		if (!dirn.exists()) {
-			logger.info("Make directory " + dirn.getAbsolutePath());
-			boolean b = dirn.mkdir();
-			if (!b) {
-				logger.error("Could not create directory, maybe parent-directories does not exist, try create parent dirs. This will fail!");
-				throw new IllegalArgumentException("Could not create directory, maybe parent-directories does not exist, try create parent dirs. This will fail!");
-			}
-		}
-		
-		OutputStream os;
-		try {
-			os = new FileOutputStream(f);
-			if (comment != null) {
-				os.write(comment.getBytes());
-				os.write('\n');
-			}
-			os.write(sb.toString().getBytes());
-			os.flush();
-			os.close();
-			logger.info("Created file " + f.getPath());
-		} catch (IOException e) {
-			logger.error("dumpToFile(Serializable rf, String id, String comment): "+stringifyException(e));
-		}
-	}
-	
 	private static byte[] readWholeFileAsBytes(String fileName) {
 		File f = new File(fileName);
 		byte[] ba = null;
@@ -100,5 +77,4 @@ public class DomainObjectSerializeUtil {
 		}
 		return ret;
 	}
-
 }
--- src/test/java/no/webtech/DeSerializeMyObject.java
+++ src/test/java/no/webtech/DeSerializeMyObject.java
@@ -4,6 +4,7 @@ import no.webtech.objectserializeutil.DomainObjectSerializeUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.Serializable;
 
 public class DeSerializeMyObject {
@@ -11,10 +12,8 @@ public class DeSerializeMyObject {
     private static Logger logger = LoggerFactory.getLogger(DeSerializeMyObject.class);
     public static void main(String[] a) {
         ExampleDomainObject e = CreateExampleDomainObject.createExampleDomainObject();
-        DomainObjectSerializeUtil.dumpToFile(e,"uniqfilenamebase","comment");
-
-        String fileName = DomainObjectSerializeUtil.serializedObjectFileName("uniqfilenamebase");
-        Serializable serializable = DomainObjectSerializeUtil.loadFromFile(fileName);
+        DomainObjectSerializeUtil.dumpToFile(e,new File("/tmp/tst.ser"),"comment");
+        Serializable serializable = DomainObjectSerializeUtil.loadFromFile("/tmp/tst.ser");
 
         System.out.println(serializable.toString());
     }
